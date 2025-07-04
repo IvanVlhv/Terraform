11e135eef18374ab8bba5ff48c2e1b2f5f78639c
Ivan <ivanvlhv@gmail.com>
2025-07-04 18:31:46 +0200

    finish vpc/main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -59,3 +59,63 @@ resource "aws_route_table" "public_route_table" {
     Name     = "public_route_table"
   }
 }
+
+# connect public subnet public_sub_1a to public route table
+resource "aws_route_table_association" "pb1a_rt_connection" {
+  subnet_id           = aws_subnet.public_sub_1a.id
+  route_table_id      = aws_route_table.public_route_table.id
+}
+
+# connect public subnet public_sub_1a to public route table
+resource "aws_route_table_association" "pb2b_rt_connection" {
+  subnet_id           = aws_subnet.public_sub_2b.id
+  route_table_id      = aws_route_table.public_route_table.id
+}
+
+# private subnet private_sub_3a
+resource "aws_subnet" "private_sub_3a" {
+  vpc_id                   = aws_vpc.vpc.id
+  cidr_block               = var.private_sub_3a
+  availability_zone        = data.aws_availability_zones.available_zones.names[0]
+  map_public_ip_on_launch  = false
+
+  tags      = {
+    Name    = "private_sub_3a"
+  }
+}
+
+# private subnet private_sub_4b
+resource "aws_subnet" "private_sub_4b" {
+  vpc_id                   = aws_vpc.vpc.id
+  cidr_block               = var.private_sub_4b
+  availability_zone        = data.aws_availability_zones.available_zones.names[1]
+  map_public_ip_on_launch  = false
+
+  tags      = {
+    Name    = "private_sub_4b"
+  }
+}
+
+# private subnet private_sub_5a
+resource "aws_subnet" "private_sub_5a" {
+  vpc_id                   = aws_vpc.vpc.id
+  cidr_block               = var.private_sub_5a
+  availability_zone        = data.aws_availability_zones.available_zones.names[0]
+  map_public_ip_on_launch  = false
+
+  tags      = {
+    Name    = "private_sub_5a"
+  }
+}
+
+# private subnet private_sub_6b
+resource "aws_subnet" "private_sub_6b" {
+  vpc_id                   = aws_vpc.vpc.id
+  cidr_block               = var.private_sub_6b
+  availability_zone        = data.aws_availability_zones.available_zones.names[1]
+  map_public_ip_on_launch  = false
+
+  tags      = {
+    Name    = "private_sub_6b"
+  }
+}
