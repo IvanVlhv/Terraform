596929d3ac022997b53864e9657ebc0c6734a175
Ivan Vlahović <ivan.vlahovic@reversinglabs.com>
2025-07-20 18:38:52 +0200

    comment fix and ami filter change

--- modules/web/main.tf
+++ modules/web/main.tf
@@ -1,11 +1,11 @@
-#this part fetches latest amaton linux ami
+#this part fetches latest amazon linux ami
 data "aws_ami" "amazon_linux" {
   most_recent = true
   owners      = ["amazon"]
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    values = ["amzn2-ami-hvm-*-x86_64-gp3"]
   }
 }
 
