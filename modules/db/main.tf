bfcfa15e845771373153b0d01eb1bd2eb6e30453
Ivan Vlahović <ivan.vlahovic@reversinglabs.com>
2025-07-23 22:27:06 +0200

    naming fix

--- modules/db/main.tf
+++ modules/db/main.tf
@@ -1,5 +1,5 @@
 resource "aws_db_subnet_group" "db_subnet_group" {
-  name       = "${var.project_name}-db-subnet-group"
+  name       = "${var.project_name}_db_subnet_group"
   subnet_ids = [var.priv_sub_db_1, var.priv_sub_db_2]
 }
 
