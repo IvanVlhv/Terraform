3291587e8dc50eedb9adcc440fe0a69b1c690b33
Ivan Vlahović <ivan.vlahovic@reversinglabs.com>
2025-07-26 21:55:31 +0200

    fix

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -19,6 +19,10 @@ resource "aws_lb_target_group" "alb_target" {
   protocol = "HTTP"
   vpc_id   = var.vpc_id
 
+  lifecycle {
+    create_before_destroy = true
+  }
+
     health_check {
     path                = "/"
     protocol            = "HTTP"
@@ -35,6 +39,10 @@ resource "aws_lb_listener" "alb_listener" {
   load_balancer_arn = aws_lb.alb.arn
   port              = 80
   protocol          = "HTTP"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 
   default_action {
     type             = "forward"
