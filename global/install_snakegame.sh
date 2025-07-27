02249cafa5e00d4a62059a003eb8e06d6fff0284
Ivan Vlahović <ivan.vlahovic@reversinglabs.com>
2025-07-27 19:17:20 +0200

    fix for bash script

--- global/install_snakegame.sh
+++ global/install_snakegame.sh
@@ -3,12 +3,11 @@ set -e
 exec > /var/log/user-data.log 2>&1
 yum update -y
 yum install -y git python3
-sudo -u ec2-user bash <<'EOF'
-cd /home/ec2-user
+cd /home
 if [ ! -d SnakeGame ]; then
   git clone https://github.com/IvanVlhv/SnakeGame.git
 fi
 cd SnakeGame
-pip3 install --user -r requirements.txt
+pip3 install -r requirements.txt
 nohup python3 app.py &
-EOF
+
